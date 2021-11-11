94e69b89e0b0e27880c56413a68d93b6e521f1d7
erickstm87 <dev@terickson.io>
2021-11-11 07:55:00 -0700

    changing

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <WebView 
+          cacheEnabled={false}
+          style={styles.container}
+          source={{uri: 'https://bike-check-92c8f.web.app/'}}
+        />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: '10%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
